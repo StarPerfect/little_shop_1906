c4886b4ee69dd210a5886789afe9025c4571bc8c
Corina Allen <StarPerfect@gmail.com>
2019-08-27 10:12:27 -0600

    Update CartsController to be using PORO Cart in #Create

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,13 @@ class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def create
+    cart = Cart.new(session[:cart])
     item = Item.find(params[:item_id])
-    item_id_str = item.id.to_s
+    # item_id_str = item.id.to_s
     cart.add_item(item.id)
     session[:cart] = cart.contents
-
-    quantity = session[:cart][item_id_str]
+    # quantity = session[:cart][item_id_str]
+    quantity = cart.contents[item.id.to_s]
     flash[:notice] = "You now have #{pluralize(quantity, "#{item.name}")} in your cart."
     redirect_to "/items"
   end
